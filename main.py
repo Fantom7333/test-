2734e43ad01dc70dfbf02a1e0e7e2ea7c65614aa
Сергей <ss@MacBook-Pro-Sergej.local>
2020-02-18 17:50:10 +0300

    new

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__, template_folder="/Users/ss/PycharmProjects/SciDive/templates")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("Главная страница.html")
+
+
+
+
+
+
+app.run(debug=True)
