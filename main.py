b4be5ecee525c621da3aa2c63b9ed8f36b2606ef
User <kkhlevnyy@mail.ru>
2020-02-19 13:39:02 +0300

    aye

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__, template_folder="/Users/ss/PycharmProjects/SciDive/templates")
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("Главная страница.html")
-
-
-
-
-
-
 app.run(debug=True)
