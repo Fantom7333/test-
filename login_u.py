30025dde853ff59bc351db48e17376cf59e5b85a
Сергей <sergey.samoylov.88@bk.ru>
2020-02-21 17:32:52 +0300

    Revert "вот так мы его"
    This reverts commit 341f649b

--- login_u.py
+++ login_u.py
@@ -1,45 +1,45 @@
 from datetime import date
-from sqlalchemy import Column, Integer, String, Boolean, Text, Date, ForeignKey, create_engine
+from sqlalchemy import (Column, Integer, String, Boolean, Text, Date, ForeignKey, create_engine)
 from sqlalchemy.orm import relationship, Session
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///info_data_base.db', echo=True)
+engine = create_engine('sqlite:///app.db', echo=True)
 Base = declarative_base(bind=engine)
 
-class Abstract:
-    id = Column(Integer, primary_key=True)
-    created_on = Column(Date, default=date.today())
 
-class User(Abstract, Base):
+
+class Main(Base):
     __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False, unique=True)
     email = Column(String(254), nullable=False, unique=True)
     password = Column(String(20), nullable=False)
-
-    tasks = relationship("Task", cascade = "all, delete-orphan")
-
+    progress = relationship("Progress", cascade="all, delete-orphan")
+    
     def __str__(self):
         return " | ".join([self.id, self.username, self.email, self.password])
 
-class Task(Abstract, Base):
-    __tablename__ = 'tasks'
-    title = Column(String(20), nullable=False, unique=True)
-    author_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    details = Column(Text)
-    deadline = Column(Date)
-    status = Column(Boolean, default=0)
 
-    author = relationship(User)
+class Progress(Base):
+    __tablename__ = 'progress'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    total_tasks_completed = Column(Integer, nullable=False)
+    score = Column(Integer(1))
+    course_name = Column(Text(100), nullable=False, unique=True)
+    total_tasks = Column(Integer, nullable=False)
+
     def __str__(self):
-        return ' | '.join([self.id, self.title, self.status])
+        return ' | '.join([self.id, self.user_id, self.total_tasks_completed, self.score])
+
 
 Base.metadata.create_all()
 
+
 def add_user(name, email, password):
-    engine = create_engine('sqlite:///info_data_base.db', echo=True)
+    engine = create_engine('sqlite:///app.db', echo=True)
     session = Session(bind=engine)
-    user = User(username=name , email=email , password=password)
+    user = Main(username=name, email=email, password=password)
     session.add(user)
     session.commit()
     session.close()
-
