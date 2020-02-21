81542e9fa8e2a7a3e887b6e211ed889cf04559f4
Сергей <sergey.samoylov.88@bk.ru>
2020-02-21 23:25:58 +0300

    Revert "вот так мы его"
    This reverts commit 341f649b

--- main.py
+++ main.py
@@ -6,17 +6,6 @@ app = Flask(__name__, template_folder="templates")
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        name = request.form['Войти']
-        if name == "Войти":
-            pass
-        else:
-            pass
+        pass
     return render_template("Главная страница.html")
-
-        # email = request.form['email']
-        # password = request.form['password']
-        # password_check = request.form['password_check']
-        # add_user(name, email, password)
-        # return redirect('/users/' + name)
-
 app.run(debug=True)
