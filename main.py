0cfc9c6d4d7865653dddbfc1286acc11e695358c
Сергей <sergey.samoylov.88@bk.ru>
2020-02-22 16:42:26 +0300

    ОБРАБОТКА КНОПОК ВХОДА/РЕГИСТРАЦИИ

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__, template_folder="templates")
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        try:
+            name = request.form["Войти"]
+        except:
+            name = request.form["Регистрация"]
+        print(name)
     return render_template("Главная страница.html")
 app.run(debug=True)
